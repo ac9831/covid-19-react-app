2606cea1b64443063a814dbff81fe29c3486b150
Lee GunJun <ac9832@naver.com>
2022-03-31 00:02:22 +0900

    Cards 코드 작성(진행중)

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup'
 
-const Cards = () => {
-    return (
-        <h1>Cards</h1>
-    )
+import styles from './Cards.module.css'
+
+const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate } } ) => {
+  if(!confirmed) {
+    return 'Loading...';
+  }
+
+  return (
+    <div classNAme={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondar" gutterBottom>Infected</Typography>
+            <Typography varaint="h5">
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">{lastUpdate}</Typography>
+            <Typography variant="body2">Number of active cases of COVID-19</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondar" gutterBottom>Recovered</Typography>
+            <Typography varaint="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">Number of recoveries from COVID-19</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondar" gutterBottom>Deaths</Typography>
+            <Typography varaint="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
 
 export default Cards
